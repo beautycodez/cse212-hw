549d1df4ff7b7bf079503607f75d37b372392683
beautycodez <angellorl_94@hotmail.com>
2025-04-14 21:45:52 -0500

    Week 06 code completed

--- week06/code/Node.cs
+++ week06/code/Node.cs
@@ -12,34 +12,65 @@ public class Node
     public void Insert(int value)
     {
         // TODO Start Problem 1
+        if (!this.Contains(value))
+        {
+            if (value < Data)
+            {
+                // Insert to the left
+                if (Left is null)
+                    Left = new Node(value);
+                else
+                    Left.Insert(value);
+            }
+            else
+            {
+                // Insert to the right
+                if (Right is null)
+                    Right = new Node(value);
+                else
+                    Right.Insert(value);
+            }
+        }
 
-        if (value < Data)
-        {
-            // Insert to the left
-            if (Left is null)
-                Left = new Node(value);
-            else
-                Left.Insert(value);
-        }
-        else
-        {
-            // Insert to the right
-            if (Right is null)
-                Right = new Node(value);
-            else
-                Right.Insert(value);
-        }
     }
 
     public bool Contains(int value)
     {
         // TODO Start Problem 2
+
+        if (value == Data)
+
+        {
+            return true; 
+        }
+        else if (value < Data && Left != null)
+        {
+            return Left.Contains(value); 
+        }
+        else if (value > Data && Right != null)
+        {
+            return Right.Contains(value); 
+        }
+
         return false;
     }
 
     public int GetHeight()
+{
+    // TODO start problem 4
+    var leftHeight = 0;
+    var rightHeight = 0;
+
+    if (Left is not null) 
     {
-        // TODO Start Problem 4
-        return 0; // Replace this line with the correct return statement(s)
+        leftHeight = Left.GetHeight(); 
     }
+
+    if (Right is not null) 
+    {
+        rightHeight = Right.GetHeight();
+    }
+
+    return 1 + Math.Max(leftHeight, rightHeight);
+}
 }
